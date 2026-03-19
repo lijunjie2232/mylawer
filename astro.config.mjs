bdddc251e7308d1d6667c12cd325590fa68385d8
lijunjie2232 <2533584225@qq.com>
2026-03-19 21:12:53 +0900

    docs

--- astro.config.mjs
+++
@@ -1,5 +0,0 @@
-import { defineConfig } from 'astro/config'
-
-export default defineConfig({
-  site: 'https://lijunjie2232.github.io/mylawer',
-})
--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -43,4 +43,5 @@ export default defineConfig({
 			],
 		}),
 	],
+	site: 'https://lijunjie2232.github.io/mylawer',
 });
